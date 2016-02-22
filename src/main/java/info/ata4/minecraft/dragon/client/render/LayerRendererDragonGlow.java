20e687b6e852bf74847ed079eaa3dfee358a8878
ata4 <barracuda415@yahoo.de>
2016-02-22 20:05:58 +0100

    Cleaned up render layers

--- src/main/java/info/ata4/minecraft/dragon/client/render/LayerRendererDragonGlow.java
+++ src/main/java/info/ata4/minecraft/dragon/client/render/LayerRendererDragonGlow.java
@@ -2,53 +2,46 @@ package info.ata4.minecraft.dragon.client.render;
 
 import info.ata4.minecraft.dragon.client.model.DragonModel;
 import info.ata4.minecraft.dragon.server.entity.EntityTameableDragon;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.entity.EntityLivingBase;
-import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Created by EveryoneElse on 14/06/2015.
  */
-public class LayerRendererDragonGlow implements LayerRenderer {
-  private final DragonRenderer dragonRenderer;
+public class LayerRendererDragonGlow implements LayerRenderer<EntityTameableDragon> {
 
-  public LayerRendererDragonGlow(DragonRenderer i_dragonRenderer)
-  {
-    dragonRenderer = i_dragonRenderer;
-  }
+    private final DragonRenderer dragonRenderer;
 
-  public void doRenderLayer(EntityTameableDragon entityDragon, float moveTime, float moveSpeed, float partialTicks,
-                            float ticksExisted, float lookYaw, float lookPitch, float scale)
-  {
-    DragonModel dragonModel = dragonRenderer.getModel();
-    dragonModel.renderPass = DragonModel.RenderPass.GLOW;
-    dragonRenderer.bindTexture(dragonModel.glowTexture);
-
-    try {
-      GL11.glPushAttrib(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_LIGHTING_BIT);
-
-      GL11.glEnable(GL11.GL_BLEND);
-      GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
-      GL11.glDisable(GL11.GL_LIGHTING);      // use full lighting
-      OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 65536, 0);
-
-      dragonModel.render(entityDragon, moveTime, moveSpeed, ticksExisted, lookYaw, lookPitch, scale);
-    } finally {
-      GL11.glPopAttrib();
+    public LayerRendererDragonGlow(DragonRenderer dragonRenderer) {
+        this.dragonRenderer = dragonRenderer;
     }
 
-  }
+    @Override
+    public void doRenderLayer(EntityTameableDragon entityDragon, float moveTime,
+            float moveSpeed, float partialTicks, float ticksExisted, float lookYaw,
+            float lookPitch, float scale) {
+        DragonModel dragonModel = dragonRenderer.getModel();
+        dragonModel.renderPass = DragonModel.RenderPass.GLOW;
+        dragonRenderer.bindTexture(dragonModel.glowTexture);
 
-  public boolean shouldCombineTextures()
-  {
-    return false;
-  }
+        try {
+            GlStateManager.pushAttrib();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GL_ONE, GL_ONE);
+            GlStateManager.disableLighting(); // use full lighting
+            
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 65536, 0);
 
-  public void doRenderLayer(EntityLivingBase entityLivingBase, float moveTime, float moveSpeed, float partialTicks,
-                            float ticksExisted, float lookYaw, float lookPitch, float scale)
-  {
-    this.doRenderLayer((EntityTameableDragon)entityLivingBase, moveTime, moveSpeed, partialTicks,
-                       ticksExisted, lookYaw, lookPitch, scale);
-  }
+            dragonModel.render(entityDragon, moveTime, moveSpeed, ticksExisted, lookYaw, lookPitch, scale);
+        } finally {
+            GlStateManager.popAttrib();
+        }
+    }
+
+    @Override
+    public boolean shouldCombineTextures() {
+        return false;
+    }
 }
--- src/main/java/info/ata4/minecraft/dragon/client/render/LayerRendererDragonSaddle.java
+++ src/main/java/info/ata4/minecraft/dragon/client/render/LayerRendererDragonSaddle.java
@@ -3,38 +3,34 @@ package info.ata4.minecraft.dragon.client.render;
 import info.ata4.minecraft.dragon.client.model.DragonModel;
 import info.ata4.minecraft.dragon.server.entity.EntityTameableDragon;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
-import net.minecraft.entity.EntityLivingBase;
 
 /**
  * Created by EveryoneElse on 14/06/2015.
  */
-public class LayerRendererDragonSaddle implements LayerRenderer {
-  private final DragonRenderer dragonRenderer;
+public class LayerRendererDragonSaddle implements LayerRenderer<EntityTameableDragon> {
 
-  public LayerRendererDragonSaddle(DragonRenderer i_dragonRenderer)
-  {
-    dragonRenderer = i_dragonRenderer;
-  }
+    private final DragonRenderer dragonRenderer;
 
-  public void doRenderLayer(EntityTameableDragon entityDragon, float moveTime, float moveSpeed, float partialTicks,
-                            float ticksExisted, float lookYaw, float lookPitch, float scale)
-  {
-    if (!entityDragon.isSaddled()) return;
-    DragonModel dragonModel = dragonRenderer.getModel();
-    dragonModel.renderPass = DragonModel.RenderPass.SADDLE;
-    dragonRenderer.bindTexture(dragonModel.saddleTexture);
-    dragonModel.render(entityDragon, moveTime, moveSpeed, ticksExisted, lookYaw, lookPitch, scale);
-  }
+    public LayerRendererDragonSaddle(DragonRenderer dragonRenderer) {
+        this.dragonRenderer = dragonRenderer;
+    }
 
-  public boolean shouldCombineTextures()
-  {
-    return false;
-  }
+    @Override
+    public void doRenderLayer(EntityTameableDragon entityDragon, float moveTime,
+            float moveSpeed, float partialTicks, float ticksExisted, float lookYaw,
+            float lookPitch, float scale) {
+        if (!entityDragon.isSaddled()) {
+            return;
+        }
+        
+        DragonModel dragonModel = dragonRenderer.getModel();
+        dragonModel.renderPass = DragonModel.RenderPass.SADDLE;
+        dragonRenderer.bindTexture(dragonModel.saddleTexture);
+        dragonModel.render(entityDragon, moveTime, moveSpeed, ticksExisted, lookYaw, lookPitch, scale);
+    }
 
-  public void doRenderLayer(EntityLivingBase entityLivingBase, float moveTime, float moveSpeed, float partialTicks,
-                            float ticksExisted, float lookYaw, float lookPitch, float scale)
-  {
-    this.doRenderLayer((EntityTameableDragon)entityLivingBase, moveTime, moveSpeed, partialTicks,
-                       ticksExisted, lookYaw, lookPitch, scale);
-  }
+    @Override
+    public boolean shouldCombineTextures() {
+        return false;
+    }
 }
