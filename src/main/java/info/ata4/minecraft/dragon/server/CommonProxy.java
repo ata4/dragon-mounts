f4d4c829c938ecc2c34467a7d3bf0ab5f0468b3d
Sunconure11 <eyespot7b@aol.com>
2017-10-30 13:37:34 -0400

    Revert "stuff"
    This reverts commit 8b293bd14590dad7806464f4577504d7f79799e5.

--- src/main/java/info/ata4/minecraft/dragon/server/CommonProxy.java
+++ src/main/java/info/ata4/minecraft/dragon/server/CommonProxy.java
@@ -10,26 +10,18 @@
 package info.ata4.minecraft.dragon.server;
 
 import info.ata4.minecraft.dragon.DragonMounts;
-import info.ata4.minecraft.dragon.core.ModBlocks;
-import info.ata4.minecraft.dragon.core.ModItems;
 import info.ata4.minecraft.dragon.server.block.BlockDragonBreedEgg;
 import info.ata4.minecraft.dragon.server.cmd.CommandDragon;
 import info.ata4.minecraft.dragon.server.entity.EntityTameableDragon;
 import info.ata4.minecraft.dragon.server.handler.DragonEggBlockHandler;
 import info.ata4.minecraft.dragon.server.item.ItemDragonBreedEgg;
-import net.minecraft.block.Block;
 import net.minecraft.command.ServerCommandManager;
-import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.event.*;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import java.lang.reflect.Field;
 
 /**
  * @author Nico Bergemann <barracuda415 at yahoo.de>
@@ -42,42 +34,6 @@ public class CommonProxy {
 	private final boolean ENTITY_SEND_VELO_UPDATES = true;
 
 	public void onPreInit(FMLPreInitializationEvent event) {
-	}
-
-	@SubscribeEvent
-	public static void registerBlocks(RegistryEvent.Register<Block> event) {
-		try {
-			for (Field f : ModBlocks.class.getDeclaredFields()) {
-				Object obj = f.get(null);
-				if (obj instanceof Block) {
-					event.getRegistry().register((Block) obj);
-				} else if (obj instanceof Block[]) {
-					for (Block block : (Block[]) obj) {
-						event.getRegistry().register(block);
-					}
-				}
-			}
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@SubscribeEvent
-	public static void registerItems(RegistryEvent.Register<Item> event) {
-		try {
-			for (Field f : ModItems.class.getDeclaredFields()) {
-				Object obj = f.get(null);
-				if (obj instanceof Item) {
-					event.getRegistry().register((Item) obj);
-				} else if (obj instanceof Item[]) {
-					for (Item item : (Item[]) obj) {
-						event.getRegistry().register(item);
-					}
-				}
-			}
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	public void onInit(FMLInitializationEvent evt) {
